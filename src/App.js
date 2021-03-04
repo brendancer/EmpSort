068e810a3f7c2c91f10b6b8f07ac18145059382f
Brenda Goodwin <brendanced@gmail.com>
2021-03-04 11:44:02 -0700

    first try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React, { useState, useEffect } from "react";
-import Table from "./components/Table";
-import Header from "./components/Header";
 import API from "./utils/API";
-import Search from "./components/Search";
+import Table from "./components/Table";
 import EmployeeRow from "./components/EmployeeRow";
+import Search from "./components/Search";
 
 function App() {
-  const [empArray, setEmpArray] = useState([]);
+  const [empArr, setEmpArr] = useState([]);
+  const [empSortArr, setEmpSortArr] = useState([]);
+  const [sortType, setSortType] = useState("name");
+  const [sortDesc, setSortDesc] = useState(false);
+  const [searchFilter, setSearchFilter] = useState("");
 
   useEffect(() => {
     API.search().then((res) => {
-      setEmpArray(res.data.results);
+      setEmpArr(res.data.results);
+      setEmpSortArr(res.data.results);
     });
   }, []);
-  console.log(empArray);
+
+  useEffect(() => {
+    const sortUp = sortDesc ? 1 : -1;
+    const sortDown = sortDesc ? -1 : 1;
+    let arr = [];
+    switch (sortType) {
+      case "picture[thumbnail]":
+        arr = [...empSortArr].sort((a, b) =>
+          a.picture.thumbnail < b.picture.thumbnail ? sortUp : sortDown
+        );
+        break;
+      case "name[first]":
+        arr = [...empSortArr].sort((a, b) =>
+          a.name.first < b.name.first ? sortUp : sortDown
+        );
+        break;
+      case "dob[date]":
+        arr = [...empSortArr].sort((a, b) =>
+          a.dob.date < b.dob.date ? sortUp : sortDown
+        );
+        break;
+      default:
+        arr = [...empSortArr].sort((a, b) =>
+          a[sortType] < b[sortType] ? sortUp : sortDown
+        );
+        break;
+    }
+    arr = [...empSortArr].filter((emp) => {
+      return (
+        emp.name.first.includes(searchFilter) ||
+        emp.name.last.includes(searchFilter) ||
+        emp.phone.includes(searchFilter) ||
+        emp.email.includes(searchFilter) ||
+        emp.dob.date.includes(searchFilter)
+      );
+    });
+    setEmpSortArr(arr);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortType, sortDesc, searchFilter]);
+
+  const handleSort = (sort) => {
+    if (sortType === sort) {
+      setSortDesc(!sortDesc);
+    } else {
+      setSortDesc(false);
+    }
+    setSortType(sort);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (e.target.value === "") {
+      setEmpSortArr(empArr);
+    } else {
+      setSearchFilter(e.target.value);
+    }
+  };
+
   return (
     <>
-      <Header />
-      <Table empArray={empArray} />
-      {/* <Search empArray={empArray} /> */}
-      {/* <EmployeeRow empArray={empArray} /> */}
+      <Search handleSearch={handleSearch} />
+      {!empSortArr.length === 0 ? (
+        <h1> Loading</h1>
+      ) : (
+        <Table handleSort={handleSort}>
+          {empSortArr.map((emp) => {
+            return <EmployeeRow key={emp.login.uuid} emp={emp} />;
+          })}
+        </Table>
+      )}
     </>
   );
 }
+
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,61 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function Table(props) {
-  const [tableArray, setTableArray] = useState([]);
-  const [firstNameArray, setFirstNameArray] = useState([]);
-  const [lastNameArray, setLastNameArray] = useState([]);
-
-  useEffect(() => {
-    setTableArray(props.empArray);
-    console.log(tableArray);
-  }, [props.empArray, tableArray]);
-
-  function handleFirstSort() {
-    let firstName = tableArray.sort((a, b) =>
-      a.name.first > b.name.first ? 1 : -1
-    );
-    setFirstNameArray(firstName);
-    setTableArray(firstNameArray);
-    console.log(tableArray);
-  }
-  function handleLastSort() {
-    let lastName = tableArray.sort((a, b) =>
-      a.name.last > b.name.last ? 1 : -1
-    );
-    setLastNameArray(lastName);
-    setTableArray(lastNameArray);
-  }
-
-  // const sortLast = (props) => {
-  //   let lastNameArray = props.empArray.sort((a, b) =>
-  //     a.name.last > b.name.last ? 1 : -1
-  //   );
-  //   setTableArray(lastNameArray);
-  // };
-
   return (
-    <>
-      <button onClick={handleFirstSort}>Sort by First Name</button>
-      <button onClick={handleLastSort}>Sort by Last Name</button>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableArray.map((employees) => (
-            <tr key={employees.login.uuid}>
-              <td>{employees.name.first}</td>
-              <td>{employees.name.last}</td>
-              <td>{employees.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">
+            <button
+              onClick={() => props.handleSort("picture[thumbnail]")}
+              className="btn"
+            >
+              <strong>Image</strong>
+            </button>
+          </th>
+          <th scope="col">
+            <button
+              onClick={() => props.handleSort("name[first]")}
+              className="btn"
+            >
+              <strong>Name</strong>
+            </button>
+          </th>
+          <th scope="col">
+            <button onClick={() => props.handleSort("phone")} className="btn">
+              <strong>Phone</strong>
+            </button>
+          </th>
+          <th scope="col">
+            <button onClick={() => props.handleSort("email")} className="btn">
+              <strong>Email</strong>
+            </button>
+          </th>
+          <th scope="col">
+            <button
+              onClick={() => props.handleSort("dob[date]")}
+              className="btn"
+            >
+              <strong>DOB</strong>
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>{props.children}</tbody>
+    </table>
   );
 }
 
